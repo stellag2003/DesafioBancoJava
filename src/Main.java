bc8cc5221895d4fc2d23ba355812ef4a66179a63
Stella Gonçalves <stellagmendonca@gmail.com>
2025-04-04 09:56:47 -0300

    desafio concluido

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("Esse é o Screen Match");
+            System.out.println("Filme: Coraline");
+
+            int anoDeLancamento = 2025;
+            System.out.println("Esse é o ano de lançamento:" + anoDeLancamento);
+
+            String sinopse = "Stella";
+
+            System.out.println(sinopse);
+
+            // essa é uma text block
+            String mensagem = """
+                  Olá, mundo!
+                  Este é um Text Block.
+                  Ele permite escrever textos com múltiplas linhas
+                  sem precisar usar caracteres de escape ou quebras de linha manualmente ou concatenações.
+                  """;
+
+        }
+    }
+}
